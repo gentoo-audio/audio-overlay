28ee3d8ed342cf3ff23af79c66eb7a702c3fce8d
Simon van der Veldt <simon.vanderveldt@gmail.com>
2017-07-12 10:12:53 +0200

    Tests: Add emerge random ebuild
    Including travis settings so it's run as part of the (daily) cron job

---
+++ tests/emerge_random_ebuild.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Emerge a random ebuild out of all the ebuilds in the overlay
+# Used to do continuous tests if our ebuilds still work
+# As well as making sure Travis's cache of the master branch is filled
+set -ex
+
+# Pick a random ebuild
+EBUILD=$(find . -regex '.*\.ebuild$' | shuf -n1)
+
+# Emerge the ebuild in a clean stage3
+docker run --rm -ti -v "${HOME}"/.portage-pkgdir:/usr/portage/packages -v "${PWD}":/usr/local/portage -w /usr/local/portage gentoo/stage3-amd64:latest /usr/local/portage/tests/emerge_ebuild.sh "${EBUILD}"
