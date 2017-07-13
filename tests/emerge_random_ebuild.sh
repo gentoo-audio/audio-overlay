6858ab706fade4344bcd4025c9805f1b68fecfc7
Simon van der Veldt <simon.vanderveldt@gmail.com>
2017-07-13 18:25:37 +0200

    Tests: fix random emerge test (#64)

--- tests/emerge_random_ebuild.sh
+++ tests/emerge_random_ebuild.sh
@@ -5,7 +5,7 @@
 set -ex
 
 # Pick a random ebuild
-EBUILD=$(find . -regex '.*\.ebuild$' | shuf -n1)
+EBUILD=$(find . -regex '.*\.ebuild$' -printf '%P\n' | shuf -n1)
 
 # Emerge the ebuild in a clean stage3
 docker run --rm -ti -v "${HOME}"/.portage-pkgdir:/usr/portage/packages -v "${PWD}":/usr/local/portage -w /usr/local/portage gentoo/stage3-amd64:latest /usr/local/portage/tests/emerge_ebuild.sh "${EBUILD}"
