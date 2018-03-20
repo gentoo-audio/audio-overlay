4ebececb987922de6cb328f0f3686770c1fa6d18
Simon van der Veldt <simon.vanderveldt@gmail.com>
2018-03-20 22:15:59 +0100

    Tests: Add random non-live ebuild emerge to daily tests

--- tests/emerge-random-live-ebuild.sh
+++ tests/emerge-random-live-ebuild.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# Emerge a random non-live ebuild out of all the ebuilds in the overlay in a clean amd64 stage3
+# Emerge a random live ebuild out of all the ebuilds in the overlay in a clean amd64 stage3
 # Used to do continuous tests if our ebuilds still work
 # As well as making sure Travis's cache of the master branch is filled
 set -ex
 
 SCRIPT_PATH=$(dirname "$0")
 
-# Pick a random non-live ebuild
-EBUILD=$(find . -regex '.*\.ebuild$' -printf '%P\n' | egrep -v '.+9999(-r[0-9]+)?.ebuild' | shuf -n1)
+# Pick a random live ebuild
+EBUILD=$(find . -regextype egrep -regex '.+9999(-r[0-9]+)?.ebuild' -printf '%P\n' | shuf -n1)
 
 ${SCRIPT_PATH}/emerge-ebuild.sh "${EBUILD}"
