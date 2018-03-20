14e18240f978bb5e26cc78efe51a654d1d0b8a1f
Simon van der Veldt <simon.vanderveldt@gmail.com>
2018-03-20 17:42:17 +0100

    Tests: deduplicate test invocation

--- tests/emerge-random-ebuild.sh
+++ tests/emerge-random-ebuild.sh
@@ -4,25 +4,9 @@
 # As well as making sure Travis's cache of the master branch is filled
 set -ex
 
+SCRIPT_PATH=$(dirname "$0")
+
 # Pick a random ebuild
 EBUILD=$(find . -regex '.*\.ebuild$' -printf '%P\n' | shuf -n1)
 
-# Create volume container named "portage" with today's gentoo tree in it
-# Ensure the portage image is up to date
-docker pull gentoo/portage
-# Clean up in case an old volume container exists
-docker rm -f portage || true
-# Create the new volume container
-docker create --name portage gentoo/portage
-
-# Ensure the stage3 image is up to date
-docker pull gentoo/stage3-amd64
-
-# Emerge the ebuild in a clean stage3
-docker run --rm -ti \
-  --cap-add=SYS_PTRACE \
-  --volumes-from portage \
-  -v "${HOME}/.portage-pkgdir":/usr/portage/packages \
-  -v "${PWD}":/usr/local/portage \
-  -w /usr/local/portage gentoo/stage3-amd64 \
-  /usr/local/portage/tests/resources/emerge-ebuild.sh "${EBUILD}"
+"${SCRIPT_PATH}/emerge-ebuild.sh" "${EBUILD}"
