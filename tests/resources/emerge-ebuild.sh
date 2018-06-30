fdbd0450df0ee486b30d5ed8d4fd88d9121e0565
Simon van der Veldt <simon.vanderveldt@gmail.com>
2018-06-30 14:05:22 +0200

    tests: allow package specific test config without version
    Both ./packages/<category>/<PN>.conf and ./packages/<category>/<P>.conf are sourced now

--- tests/resources/emerge-ebuild.sh
+++ tests/resources/emerge-ebuild.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
-# Tests emering of an ebuild
-# Installs dependencies first
-# Can use predefined env vars stored in ./packages/<category>/<ebuild>.env
+# Tests emering of an ebuild, installs dependencies first
+# Takes the path to an ebuild as the one and only argument
+# Sources ebuild specific config from ./packages/<category>/<PN>.conf and
+# ./packages/<category>/<P>.conf if those files exist
+# Depends on qatom from app-portage/portage-utils
 set -ex
 
 if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <ebuild category>/<ebuild name>.ebuild" >&2
+  echo "Usage: $0 <ebuild category>/<ebuild name>/<ebuild filename>.ebuild" >&2
   exit 1
 fi
 
-EBUILD="${1}"
-echo "Emerging ${EBUILD}"
+EBUILD_PATH="${1}"
 
 # Disable news messages from portage and disable rsync's output
 export FEATURES="binpkg-multi-instance -news" PORTAGE_RSYNC_EXTRA_OPTS="-q"
+
+echo "Emerging dependencies"
+emerge -q --buildpkg --usepkg app-portage/portage-utils
+
+EBUILD_FILENAME=$(basename "${EBUILD_PATH}" ".ebuild")
+EBUILD_CATEGORY="${EBUILD_PATH%%/*}"
+EBUILD="${EBUILD_CATEGORY}/${EBUILD_FILENAME}"
+echo "Emerging ${EBUILD}"
 
 # Set portage's distdir to /tmp/distfiles
 # This is a workaround for a bug in portage/git-r3 where git-r3 can't
@@ -32,15 +41,20 @@ EOF
 # Source ebuild specific env vars
 unset ACCEPT_KEYWORDS
 unset USE
-PKG_NAME=$(basename "${EBUILD}" ".ebuild")
-PKG_CATEGORY="${EBUILD%%/*}"
-PKG_CONF_FILE="./tests/resources/packages/${PKG_CATEGORY}/${PKG_NAME}.conf"
-if [ -f "${PKG_CONF_FILE}" ]; then
-  source "${PKG_CONF_FILE}"
+PKG_CATEGORY=$(qatom -F "%{CATEGORY}" ${EBUILD})
+PKG_NAME=$(qatom -F "%{PN}" ${EBUILD})
+PKG_FULL_NAME=$(qatom -F "%{PF}" ${EBUILD})
+# Source versionless config
+if [ -f "./tests/resources/packages/${PKG_CATEGORY}/${PKG_NAME}.conf" ]; then
+  source "./tests/resources/packages/${PKG_CATEGORY}/${PKG_NAME}.conf"
+fi
+# Source version specific config
+if [ -f "./tests/resources/packages/${PKG_CATEGORY}/${PKG_FULL_NAME}.conf" ]; then
+  source "./tests/resources/packages/${PKG_CATEGORY}/${PKG_FULL_NAME}.conf"
 fi
 
 # Emerge dependencies first
-emerge --quiet-build --buildpkg --usepkg --onlydeps --autounmask=y --autounmask-continue=y "=${PKG_CATEGORY}/${PKG_NAME}"
+emerge --quiet-build --buildpkg --usepkg --onlydeps --autounmask=y --autounmask-continue=y "=${PKG_CATEGORY}/${PKG_FULL_NAME}"
 
 # Emerge the ebuild itself
-emerge -v "=${PKG_CATEGORY}/${PKG_NAME}"
+emerge -v "=${PKG_CATEGORY}/${PKG_FULL_NAME}"
