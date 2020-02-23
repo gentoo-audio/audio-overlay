eac9854bb19c7e5582d58818e39a9f4ad5d15da7
Simon van der Veldt <simon.vanderveldt@gmail.com>
2020-02-23 15:55:24 +0100

    tests: Add tests to emerge all ebuilds
    Split in two, one for emering all non-live (i.e. stable and testing) ebuilds and another to emerge all live ebuilds.
    This will emerge all ebuilds in one go in a container meaning it will be faster than emerging all of them in a separate container but it's slightly less deterministic because the install from one of the earlier ebuilds might impact/interfere with the install of a later ebuild.
    Because of this the test configs have been adapted as well to all write their configs to the same file to ensure that there are no USE flag blockers

---
+++ tests/emerge-all-ebuilds.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Get all non-live ebuilds that are in the git tree and emerge all of them in a clean amd64 stage3
+set -e
+
+if [ "${DEBUG}" = True ]; then
+  set -x
+fi
+
+SCRIPT_PATH=$(dirname "$0")
+
+EBUILDS=($(git ls-files | grep -e "\.ebuild$" | egrep -v '.+9999(-r[0-9]+)?.ebuild'))
+
+echo "Emerging the following ebuilds: ${EBUILDS[@]}"
+
+# Emerge the ebuilds in a clean stage3
+"${SCRIPT_PATH}/emerge-ebuild.sh" "${EBUILDS[@]}"
